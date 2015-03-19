a2ce68d2b4462f271a045a1aa6874bb837281cdc
Steffen Köhler <steffen.koehler@stud.hs-mannheim.de>
2015-03-19 19:43:32 +0100

    C# PasswortGenerator

---
+++ C#/PasswordGenerator/PasswordGenerator/Crypter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    class Crypter
+    {
+        public static void setPasswort(string passwort)
+        {
+
+        }
+    }
+}
---
+++ C#/PasswordGenerator/PasswordGenerator/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+ 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void PasswortTextBox_TextChanged(object sender, EventArgs e)
+        {
+            generieren.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String passwort = PasswortTextBox.Text;
+            Crypter.setPasswort(passwort);
+        }
+
+        private void Account_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Account.Items.Add("steffen-koehler1@gmx.de");
+        }
+    }
+}
